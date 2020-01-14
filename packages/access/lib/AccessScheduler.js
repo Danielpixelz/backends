7478d32f4ab9a32711fa1258465de786bc1fbfe6
Patrick Venetz <venetz@gmail.com>
2020-01-14 13:14:59 +0100

    feat(access): Expose scheduler jobs expireGrants, followupGrants

--- packages/access/lib/AccessScheduler.js
+++ packages/access/lib/AccessScheduler.js
@@ -4,8 +4,8 @@ const debug = require('debug')('access:lib:accessScheduler')
 const PgDb = require('@orbiting/backend-modules-base/lib/PgDb')
 const Redis = require('@orbiting/backend-modules-base/lib/Redis')
 
-const campaignsLib = require('./campaigns')
-const grantsLib = require('./grants')
+const expireGrants = require('./expireGrants')
+const followupGrants = require('./followupGrants')
 
 // Interval in which scheduler runs
 const intervalSecs = 60 * 10
@@ -34,8 +34,8 @@ const init = async ({ t, mail }) => {
     try {
       const lock = await schedulerLock(redis).lock(LOCK_KEY, 1000 * intervalSecs)
 
-      await expireGrants(t, pgdb, mail)
-      await followupGrants(t, pgdb, mail)
+      await expireGrants({}, { t, pgdb, mail })
+      await followupGrants({}, { t, pgdb, mail })
 
       // Extend lock for a fraction of usual interval to prevent runner to
       // be executed back-to-back to previous run.
@@ -79,49 +79,3 @@ const init = async ({ t, mail }) => {
 }
 
 module.exports = { init }
-
-/**
- * Renders expired grants invalid.
- */
-const expireGrants = async (t, pgdb, mail) => {
-  debug('expireGrants...')
-  for (const grant of await grantsLib.findInvalid(pgdb)) {
-    const transaction = await pgdb.transactionBegin()
-
-    try {
-      await grantsLib.invalidate(grant, 'expired', t, transaction, mail)
-      await transaction.transactionCommit()
-    } catch (e) {
-      await transaction.transactionRollback()
-
-      debug('rollback', { grant: grant.id })
-
-      throw e
-    }
-  }
-  debug('expireGrant done')
-}
-
-/**
- * Runs follow-up on invalidated grants.
- */
-const followupGrants = async (t, pgdb, mail) => {
-  debug('followupGrants...')
-  for (const campaign of await campaignsLib.findAll(pgdb)) {
-    for (const grant of await grantsLib.findEmptyFollowup(campaign, pgdb)) {
-      const transaction = await pgdb.transactionBegin()
-
-      try {
-        await grantsLib.followUp(campaign, grant, t, transaction, mail)
-        await transaction.transactionCommit()
-      } catch (e) {
-        await transaction.transactionRollback()
-
-        debug('rollback', { grant: grant.id })
-
-        throw e
-      }
-    }
-  }
-  debug('followupGrants done')
-}
